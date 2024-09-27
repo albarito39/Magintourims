8cb872791f0b83e100e7d5b34e1f423d55df1026
albarito39 <cuadradojuanfer@gmail.com>
2024-09-27 15:16:59 -0500

    termnacion de hogar e comienzo de info

--- frontend/src/components/informacion.jsx
+++ frontend/src/components/informacion.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import "../styles/main.css";
 
-function Informacion() {
-  return(
-<div className="m-0 d-flex vh-100" color="grey"></div>
-  )  
+function Informacion(props) {
+  return (
+    <div
+      className="modal fade show d-block ver_mas"
+      id="exampleModalToggle"
+      aria-hidden="true"
+      aria-labelledby="exampleModalToggleLabel"
+      tabIndex="-1"
+      style={{ display: "block" }}
+    >
+      <div className="m-3 p-5 d-block vh-100 card_informacion">
+        <div className="">
+          <button onClick={props.close}>close</button>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, ab
+          laborum? Delectus, hic repellat vitae voluptas, corporis facilis
+          mollitia ullam adipisci necessitatibus natus consequuntur laudantium
+          saepe voluptatem labore inventore rem?
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Informacion
+export default Informacion;
